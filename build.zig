0301754c6b98c19ca4af559cd73cd5dca919db5f
zed <dee0xeed@gmail.com>
2024-08-08 14:23:27 +0300

    On branch main, 0.12 -> 0.14  modified:   .gitignore  new file:   MuP21.txt  modified:   build.zig

--- build.zig
+++ build.zig
@@ -2,20 +2,12 @@ const std = @import("std");
 
 pub fn build(b: *std.Build) void {
 
-//    const target = b.standardTargetOptions(.{});
-//    const mode = b.standardReleaseOptions();
-//    const exe = b.addExecutable("zf", "src/main.zig");
-//    exe.single_threaded = true;
-//    exe.setTarget(target);
-//    exe.setBuildMode(mode);
-//    exe.install();
-
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
     const exe = b.addExecutable(.{
         .name = "zf",
-        .root_source_file = .{ .path = "src/main.zig" },
+        .root_source_file = b.path("src/main.zig"),
         .target = target,
         .optimize = optimize,
         .single_threaded = true,
